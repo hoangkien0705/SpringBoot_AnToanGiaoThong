0baaa924afec2642201661208d3aa92658c5e76e
hoangkien0705 <hoangkien0705@gmail.com>
2017-07-20 20:09:50 +0700

    delete

--- src/main/java/com/cervus/nippon/antoangiaothong/repository/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cervus.nippon.antoangiaothong.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cervus.nippon.antoangiaothong.entity.Role;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-
-}
--- src/main/java/com/cervus/nippon/common/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.cervus.nippon.common;
-
-public class Constants {
-	
-	public static final boolean DEBUG = true;
-	
-}
