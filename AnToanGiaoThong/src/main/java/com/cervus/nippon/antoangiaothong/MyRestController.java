4f470163af4f2068c8955f4d62e6798ad5b1f227
Hoangkien0705 <hoangkien0705@gmail.com>
2017-10-07 11:09:56 +0700

    commit

--- AnToanGiaoThong/src/main/java/com/cervus/nippon/antoangiaothong/MyRestController.java
+++ AnToanGiaoThong/src/main/java/com/cervus/nippon/antoangiaothong/MyRestController.java
@@ -1,25 +1,16 @@
 package com.cervus.nippon.antoangiaothong;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.cervus.nippon.antoangiaothong.entity.User;
-import com.cervus.nippon.antoangiaothong.repository.UserRepo;
 
 @RestController
 public class MyRestController {
 	
-	@Autowired
-	private UserRepo uRepo;
 	
 	@GetMapping("/" )
 	public String[] home(HttpServletRequest request) {
-		List<User> list = uRepo.findAll();
 		return new String[] { "hoang van kien", "hoang van kien", "hoang van kien" };
 	}
 }
