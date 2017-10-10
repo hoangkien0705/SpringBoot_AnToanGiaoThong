b045df9e49244c0dd5f39ea6b4c16f8cf2ace6e4
Hoangkien0705 <hoangkien0705@gmail.com>
2017-10-10 20:57:55 +0700

    commit

---
+++ AnToanGiaoThong/src/main/resources/public/js/login.js
@@ -0,0 +1,17 @@
+var KhoaPham = React.createClass({
+    render : function () {
+        return (
+            <div>
+                    <h1>Khoa Pham React JS</h1>
+                    <h1>Khoa Pham React JS</h1>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <div>
+    	<h2>Default</h2>
+        <KhoaPham/>
+    </div>	
+    , document.getElementById("root"));
